8e013e2af3f3e487cc418c3d7d4289dee8f8d46d
BJ Hansen <bjhansen@ualr.edu>
2017-06-02 13:14:44 -0500

    Installation updates
    Removed run.cmd which is now auto generated via the install.cmd command.

--- install.cmd
+++ install.cmd
@@ -1,3 +1,11 @@
 @echo off
 
 mklink /D %~dp0\vendor %~dp0\assets\laravel\iotmgmt\vendor
+
+
+
+echo @echo off >> %~dp0\run.cmd
+
+echo assets\php\windows\php.exe assets\laravel\iotmgmt\artisan iot:register >> %~dp0\run.cmd
+
+echo pause >> %~dp0\run.cmd
--- run.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-assets\php\windows\php.exe assets\laravel\iotmgmt\artisan iot:register
-
-pause
